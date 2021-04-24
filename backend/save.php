1fdb50f6534bfb89318ac5a55f62b7dbfa98c70e
api-application <51210148+api-application@users.noreply.github.com>
2021-04-24 06:49:34 +0100

    Update save.php

--- backend/save.php
+++ backend/save.php
@@ -3,13 +3,13 @@ include 'database.php';
 if(count($_POST)>0){
 	if($_POST['type']==1){
 		$name=$_POST['name'];
-	$email=$_POST['email'];
-	$phone=$_POST['phone'];
-	$city=$_POST['city'];
-	$sql = "INSERT INTO `crud`( `name`, `email`, `phone`, `city`) 
-	VALUES ('$name','$email','$phone','$city')";
-	if (mysqli_query($conn, $sql)) {
-		echo json_encode(array("statusCode"=>200));
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$city=$_POST['city'];
+		$sql = "INSERT INTO `crud`( `name`, `email`,`phone`,`city`) 
+		VALUES ('$name','$email','$phone','$city')";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
 		} 
 		else {
 			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
